e59eba856e936e4f401048f1adc63bb706798bd9
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-04-27 13:24:41 +0700

    start start animation state implementation

--- Source/MultiplayerFPSPogU/FPSGameplayTags.cpp
+++ Source/MultiplayerFPSPogU/FPSGameplayTags.cpp
@@ -5,4 +5,5 @@
 namespace FPSGameplayTags
 {
     UE_DEFINE_GAMEPLAY_TAG_COMMENT(Event_Movement_ADS, "Event.Movement.ADS", "Aim down Sights event");
+    UE_DEFINE_GAMEPLAY_TAG_COMMENT(Event_Movement_Melee, "Event.Movement.Melee", "Melee event");
 }
