f97fb06639d677898dfdd17a2791e07e7b62d67e
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-04-28 20:03:11 +0700

    Setup jog state for walking && crouching && ads

---
+++ Source/MultiplayerFPSPogU/Public/Animation/AnimDataTypes.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "Animation/AnimSequence.h"
+#include "AnimDataTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCardinalDirections
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations")
+    TObjectPtr<UAnimSequence> Forward;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations")
+    TObjectPtr<UAnimSequence> Backward;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations")
+    TObjectPtr<UAnimSequence> Left;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations")
+    TObjectPtr<UAnimSequence> Right;
+};
+
+UENUM(BlueprintType)
+enum class ECardinalDirection : uint8
+{
+    Forward = 0,
+    Backward,
+    Right,
+    Left
+};
+
+UENUM(BlueprintType)
+enum class ERootYawOffsetModeType : uint8
+{
+    BlendOut = 0,
+    Hold,
+    Accumulate
+};
+
